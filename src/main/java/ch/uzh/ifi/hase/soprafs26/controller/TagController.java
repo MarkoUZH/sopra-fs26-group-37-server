bb2981327060efc57dbb8d71381362853db1f06b
Marko Milojevic <marko.milojevic@uzh.ch>
2026-04-19 22:35:22 +0200

    feat: implemented Crud methods for Tag entity #78

--- src/main/java/ch/uzh/ifi/hase/soprafs26/controller/TagController.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs26/controller/TagController.java
@@ -1,37 +1,92 @@
 package ch.uzh.ifi.hase.soprafs26.controller;
 
+import ch.uzh.ifi.hase.soprafs26.rest.dto.TagGetDTO;
+import ch.uzh.ifi.hase.soprafs26.rest.dto.TagPostDTO;
+import ch.uzh.ifi.hase.soprafs26.rest.mapper.TagDTOMapper;
 import ch.uzh.ifi.hase.soprafs26.service.UserService;
 
 
 import ch.uzh.ifi.hase.soprafs26.entity.Tag;
 import ch.uzh.ifi.hase.soprafs26.service.TagService;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 public class TagController {
 
-	private final TagService tagService;
+    private final TagService tagService;
     private final UserService userService;
 
-	TagController(TagService tagService, UserService userService) {
-		this.tagService = tagService;
+    TagController(TagService tagService, UserService userService) {
+        this.tagService = tagService;
         this.userService = userService;
-	}
+    }
 
     @GetMapping("/tags")
     @ResponseStatus(HttpStatus.OK)
     @ResponseBody
-    public List<Tag> getAllTags(@RequestHeader(value = "Authorization", required = false) String token) {
-        
+    public List<TagGetDTO> getAllTags(
+            @RequestHeader(value = "Authorization", required = false) String token) {
+
         userService.verifyToken(token);
 
-        return null;
+        return tagService.getTags().stream()
+                .map(TagDTOMapper.INSTANCE::convertEntityToTagGetDTO)
+                .toList();
+    }
+
+    @GetMapping("/tags/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    public TagGetDTO getTagById(
+            @RequestHeader(value = "Authorization", required = false) String token,
+            @PathVariable Long id) {
+
+        userService.verifyToken(token);
+
+        Tag tag = tagService.getTagById(id);
+        return TagDTOMapper.INSTANCE.convertEntityToTagGetDTO(tag);
+    }
+
+    @PostMapping("/tags")
+    @ResponseStatus(HttpStatus.CREATED)
+    @ResponseBody
+    public TagGetDTO createTag(
+            @RequestHeader(value = "Authorization", required = false) String token,
+            @RequestBody TagPostDTO tagPostDTO) {
+
+        userService.verifyToken(token);
+
+        Tag tag = TagDTOMapper.INSTANCE.convertTagPostDTOtoEntity(tagPostDTO);
+        Tag createdTag = tagService.createTag(tag);
+        return TagDTOMapper.INSTANCE.convertEntityToTagGetDTO(createdTag);
+    }
+
+    @PutMapping("/tags/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    public TagGetDTO updateTag(
+            @RequestHeader(value = "Authorization", required = false) String token,
+            @PathVariable Long id,
+            @RequestBody TagPostDTO tagPostDTO) {
+
+        userService.verifyToken(token);
+
+        Tag tag = TagDTOMapper.INSTANCE.convertTagPostDTOtoEntity(tagPostDTO);
+        Tag updatedTag = tagService.updateTag(id, tag);
+        return TagDTOMapper.INSTANCE.convertEntityToTagGetDTO(updatedTag);
+    }
+
+    @DeleteMapping("/tags/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public TagGetDTO deleteTag(
+            @RequestHeader(value = "Authorization", required = false) String token,
+            @PathVariable Long id) {
+
+        userService.verifyToken(token);
+
+        return TagDTOMapper.INSTANCE.convertEntityToTagGetDTO(tagService.deleteTag(id));
     }
 }
--- src/main/java/ch/uzh/ifi/hase/soprafs26/service/TagService.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs26/service/TagService.java
@@ -1,21 +1,80 @@
 package ch.uzh.ifi.hase.soprafs26.service;
 
+import ch.uzh.ifi.hase.soprafs26.entity.Tag;
 import ch.uzh.ifi.hase.soprafs26.repository.TagRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 @Service
 @Transactional
 public class TagService {
 
-	private final Logger log = LoggerFactory.getLogger(TagService.class);
+    private final Logger log = LoggerFactory.getLogger(TagService.class);
 
-	private final TagRepository tagRepository;
+    private final TagRepository tagRepository;
 
-	public TagService(@Qualifier("tagRepository") TagRepository tagRepository) {
-		this.tagRepository = tagRepository;
-	}
+    public TagService(@Qualifier("tagRepository") TagRepository tagRepository) {
+        this.tagRepository = tagRepository;
+    }
+
+    public List<Tag> getTags() {
+        return tagRepository.findAll();
+    }
+
+    public Tag getTagById(Long id) {
+        log.debug("Fetching tag with id {}", id);
+        return tagRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,
+                        "Tag with id " + id + " not found"));
+    }
+
+    public Tag createTag(Tag tag) {
+        log.debug("Creating tag with name {}", tag.getName());
+        validateTagName(tag);
+        Tag savedTag = tagRepository.save(tag);
+        tagRepository.flush();
+        return savedTag;
+    }
+
+    public Tag updateTag(Long id, Tag updatedTag) {
+        log.debug("Updating tag with id {}", id);
+        Tag existingTag = getTagById(id);
+
+        if (updatedTag.getName() != null) {
+            existingTag.setName(updatedTag.getName());
+        }
+        if (updatedTag.getProject() != null) {
+            existingTag.setProject(updatedTag.getProject());
+        }
+        if (updatedTag.getTasks() != null) {
+            existingTag.setTasks(updatedTag.getTasks());
+        }
+
+        Tag savedTag = tagRepository.save(existingTag);
+        tagRepository.flush();
+        return savedTag;
+    }
+
+    public Tag deleteTag(Long id) {
+        log.debug("Deleting tag with id {}", id);
+        Tag deletedTag = getTagById(id);
+        tagRepository.deleteById(id);
+        tagRepository.flush();
+
+        return deletedTag;
+    }
+
+    private void validateTagName(Tag tag) {
+        if (tag.getName() == null || tag.getName().isBlank()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
+                    "Tag name must not be empty");
+        }
+    }
 }
